5cfb63dade75506413732a309257bd13ad85d677
Mohammad hmedat <humedat23@gmail.com>
2025-06-18 23:47:26 +0300

    feat(App lifecyle demo): Add init file for implementing lifecycle states of mobile app

---
+++ flutter_ui_samples/lib/app_lifecycle_demo/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppLifecycleDemo extends StatefulWidget {
+  const AppLifecycleDemo({super.key});
+
+  @override
+  State<AppLifecycleDemo> createState() => _AppLifecycleDemoState();
+}
+
+class _AppLifecycleDemoState extends State<AppLifecycleDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
